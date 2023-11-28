1607a9ea227764589d41847466ceeb5ffaab9f47
pk <aungpyaekyaw@ucsm.edu.mm>
2023-11-28 18:03:40 +0630

    init.

--- complete/src/main/java/com/example/springboot/HelloController.java
+++ complete/src/main/java/com/example/springboot/HelloController.java
@@ -3,12 +3,32 @@ package com.example.springboot;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.sql.*;
+
 @RestController
 public class HelloController {
 
 	@GetMapping("/")
 	public String index() {
-		return "Greetings from Spring Boot!";
+		String url = "jdbc:postgresql://localhost:5432/postgres?currentSchema=volga&stringtype=unspecified";
+		String username = "ni";
+		String password = "ni";
+		try (Connection connection =
+					 DriverManager.getConnection(
+							 url, username, password);
+			 Statement statement = connection.createStatement();
+			 ResultSet resultSet = statement.executeQuery("select 'hello' ")) {
+			if (resultSet.next()) {
+				return "UP";
+			} else {
+				return "Down";
+			}
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+			return "Error";
+		}
 	}
+
+
 
 }
